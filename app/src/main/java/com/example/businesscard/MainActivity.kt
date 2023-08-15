9dae85fae24658fbbe54777ab6f1df4495f215b0
austinstutzman <alsghs49@gmail.com>
2023-08-14 21:55:18 -0400

    completing first iteration of business card.
    VERY BASIC color scheme, focusing mostly on the layout which I got to match the project original

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,13 +3,35 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +44,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BusinessCardApp()
                 }
             }
         }
@@ -30,17 +52,84 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BusinessCardApp(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(Color(0xFFFFAF42)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Spacer(modifier = modifier)
+        MainContent(
+            painterResource(R.drawable.ic_launcher_foreground),
+            "Austin Stutzman",
+            "Software Engineer",
+        )
+        ContactInfo()
+    }
 }
+
+@Composable
+fun MainContent(painter: Painter, fullName: String, title: String) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = null,
+            modifier = Modifier.padding(8.dp)
+        )
+        Text(
+            fullName,
+            fontSize = 32.sp,
+            color = Color(0xFFFF8303),
+            modifier = Modifier.padding(start = 8.dp, end = 8.dp)
+        )
+        Text(
+            title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun ContactInfo() {
+    Column(
+        modifier = Modifier.padding(24.dp)
+    ) {
+        ContactInfoDetail(Icons.Default.Phone, stringResource(R.string.phone_number))
+        ContactInfoDetail(Icons.Default.Share, stringResource(R.string.socials_link))
+        ContactInfoDetail(Icons.Default.Email, stringResource(R.string.email))
+    }
+}
+
+@Composable
+fun ContactInfoDetail(imageVector: ImageVector, text: String) {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        Icon(
+            imageVector,
+            contentDescription = null,
+            modifier = Modifier.padding(4.dp),
+        )
+        Text(
+            text = text,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BusinessCardAppPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        BusinessCardApp()
     }
 }
